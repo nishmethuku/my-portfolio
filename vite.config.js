0a5fe1be0879d7491a4863d87bd0afa5c5092435
Nish Methuku <nishm0411@gmail.com>
2025-08-05 21:56:24 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
  
 export default defineConfig({
   plugins: [react()],
+  base: '/my-portfolio/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 }); 
