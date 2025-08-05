7f5a6f84272465111cb2ddd5d521b29e2555e635
Nish Methuku <nishm0411@gmail.com>
2025-08-05 22:14:37 +0530

    Fix Vite base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
  
 export default defineConfig({
+  base: './', // ✅ THIS is the fix
   plugins: [react()],
-  base: '/my-portfolio/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
   }
-}); 
+});
